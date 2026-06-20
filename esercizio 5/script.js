bf03d5f1b9fba6b73ea4c8a6f1b796fa4824bb33
chrisxwave <chrisilmer914@gmail.com>
2026-06-20 14:23:34 +0200

    commit del 5 esercizio

---
+++ esercizio 5/script.js
@@ -0,0 +1,6 @@
+const nums = [2, 8, 4, 7, 12, 87];
+
+const pari = nums.filter(num => num % 2 === 0);
+console.log(pari);
+// Crea un array a partire dall'array nums, che abbia solo i numeri pari
+// Risultato: [2, 8, 4, 12]
