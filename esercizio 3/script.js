0c7886097cca1d398a325edd51f7e3de42a9bc4d
chrisxwave <chrisilmer914@gmail.com>
2026-06-20 13:35:43 +0200

    commit del 3 esercizio

---
+++ esercizio 3/script.js
@@ -0,0 +1,8 @@
+const numbers = [2, 8, 4, 7, 2, 87];
+
+const aumentati = numbers.map(numbers=>numbers + 1);
+
+console.log(aumentati);
+
+// Crea un nuovo array in cui tutti i numeri siano incrementati di 1
+// Risultato: [3, 9, 5, 8, 3, 88]
