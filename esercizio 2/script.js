40af986bdaa003acab13e8024ac3aae3ffd1934c
chrisxwave <chrisilmer914@gmail.com>
2026-06-20 13:24:36 +0200

    commit esercizio 2

---
+++ esercizio 2/script.js
@@ -0,0 +1,11 @@
+const people = [
+  { name: 'Paolo', age: 35 },
+  { name: 'Giulia', age: 24 },
+  { name: 'Marco', age: 67 }
+];
+
+const names = people.map(person => person.name);
+
+console.log(names);
+// Stampa in console tutti i nomi
+// Risultato: 'Paolo', 'Giulia', 'Marco'
